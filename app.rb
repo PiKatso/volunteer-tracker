93849c3ba10cb63444924c81d847038042db936c
Kat Gifford <katgiff2007@gmail.com>
2017-05-05 14:18:51 -0700

    add collecting input from index data forms and texted

--- app.rb
+++ app.rb
@@ -8,3 +8,19 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+post('/add-project') do
+  project_name = params['project-name']
+  project_description = params['project-description']
+  #need to clean string/check valid input still
+  Project.add(project_name, project_description)
+  erb(:index)
+end
+
+post('/add-volunteer') do
+  first_name = params['first-name']
+  last_name = params['last-name']
+  volunteer_name = first_name + ' ' + last_name
+  Volunteer.add(volunteer_name)
+  erb(:index)
+end

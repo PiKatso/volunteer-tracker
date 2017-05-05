e926f7461c026987b17626e2c5c3d82662031188
Kat Gifford <katgiff2007@gmail.com>
2017-05-05 10:52:54 -0700

    add second passing spec for projects table .add method

--- lib/volunteer_tracker.rb
+++ lib/volunteer_tracker.rb
@@ -6,7 +6,15 @@ DB = PG.connect({:dbname => 'volunteer_tracker'})
 class Volunteer
 
   def self.add(name)
-    DB.exec("INSERT INTO volunteers VALUES (uuid_generate_v4(), '#{name.downcase}') RETURNING id;") 
+    DB.exec("INSERT INTO volunteers VALUES (uuid_generate_v4(), '#{name.downcase}') RETURNING id;")
+  end
+
+end
+
+class Project
+
+  def self.add(name, description)
+    DB.exec("INSERT INTO projects VALUES (uuid_generate_v4(), '#{name.downcase}', '#{description.downcase}') RETURNING id;")
   end
 
 end
--- spec/volunteer_tracker_spec.rb
+++ spec/volunteer_tracker_spec.rb
@@ -3,9 +3,18 @@ require "spec_helper"
 describe 'Volunteer' do
 
   describe '#add' do
-    it "adds a new volunteer to the volunteers database" do
+    it "adds a new volunteer to the volunteers table in database" do
       expect(Volunteer.add('becky')[0]['id']).to match(UUID_MATCHER)
     end
   end
 
+end
+
+describe 'Project' do
+
+  describe '#add' do
+    it "adds a new project to the projects table in database" do
+      expect(Project.add("habitat for humanity", 'repairing a home')[0]['id']).to match(UUID_MATCHER)
+    end
+  end
 end

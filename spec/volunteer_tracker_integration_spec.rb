1859ec590a56ddfe3d2a0e8c80ad1dd893c13936
Kat Gifford <katgiff2007@gmail.com>
2017-05-10 21:55:58 -0700

    add integration spec

--- spec/volunteer_tracker_integration_spec.rb
+++ spec/volunteer_tracker_integration_spec.rb
@@ -5,6 +5,7 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe("the project path", {:type => :feature}) do
+
   it("creates a project and displays project info on the project page") do
     visit("/")
     fill_in("project-name", :with => "volunteer")
@@ -12,4 +13,38 @@ describe("the project path", {:type => :feature}) do
     click_button("Create Project")
     expect(page).to have_content()
   end
+
+  # it("will click a project button at top and go to page that holds all projects") do
+  #   visit("/")
+  #   fill_in("project-name", :with => "Beach clean up")
+  #   fill_in("project-description", :with => "Do stuff")
+  #   click_button("Create Project")
+  #   visit('/projects')
+  #   click_link("Beach clean up")
+  #   expect(page).to have_content("All Projects: Beach clean up Do stuff")
+  #   click_button("Home")
+  # end
+
+end
+
+describe("the volunteer path", {:type => :feature}) do
+
+  it("creates a volunteer and displays project info on the project page") do
+    visit("/")
+    fill_in("first-name", :with => "Ben")
+    fill_in("last-name", :with => "Stiller")
+    click_button("Add Volunteer")
+    expect(page).to have_content()
+  end
+
+  # it("will click a project button at top and go to page that holds all projects") do
+  #   visit("/")
+  #   fill_in("first-name", :with => "Ben")
+  #   fill_in("last-name", :with => "Stiller")
+  #   click_button("Add Volunteer")
+  #   visit('/volunteers')
+  #   click_link("Ben Stiller")
+  #   expect(page).to have_content("Ben Stiller")
+  # end
+
 end
